1bb51a2a26c6e7b3f0d2a7732126cbe06be8135e
Willian <willian.m.sousa@gmail.com>
2023-05-01 19:26:36 -0300

    Skill section, fonts

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 import 'react-multi-carousel/lib/styles.css';
 import { Container, Row, Col } from "react-bootstrap";
 import skillImg from "../assets/img/skill-img.svg";
+import meter from "../assets/img/meter1.svg"
 import TrackVisibility from 'react-on-screen';
 
 
@@ -28,38 +29,38 @@ export const Skills = () => {
   return (
     <section className="skill" id="skills">
       <Container>
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="skill-bx wow zoomIn">
-                <p>Da configuração a otimização! Esse é o #social o plano pensado para facilitar o
-                  dia a dia da comunicação nas redes sociais  da sua empresa!</p>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12">
-              <div className="skill-bx2 wow zoomIn">
-                <p id="corel1">Usamos nossas experiências dos últimos 2 anos e mais de 30 empresas testadas para
-                  criar um serviço que realmente funciona de acordo com a necessidade da sua empresa.</p>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12">
-              <div className="skill-bx2 wow zoomIn">
-                <p id="corel1">O controle nas suas mãos! Elaboramos os relatórios de todo o processo, com definição
-                  de metas com foco em resultados.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <Row className="aligh-items-center">
+        <Row className="justify-content-md-center">
           <Col xs={12} md={6} xl={5}>
+            <div className="row">
+              <div className="col-12">
+                <div className="skill-bx wow zoomIn">
+                  <p>Da configuração a otimização! Esse é o #social o plano pensado para facilitar o
+                    dia a dia da comunicação nas redes sociais  da sua empresa!</p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div className="skill-bx2 wow zoomIn">
+                  <p id="corel1">Usamos nossas experiências dos últimos 2 anos e mais de 30 empresas testadas para
+                    criar um serviço que realmente funciona de acordo com a necessidade da sua empresa.</p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div className="skill-bx2 wow zoomIn">
+                  <p id="corel1">O controle nas suas mãos! Elaboramos os relatórios de todo o processo, com definição
+                    de metas com foco em resultados.</p>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} md={6} xl={5} className='mt-5'>
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={skillImg} alt="Header Img" />
+                  <img src={skillImg} alt="Skill Img" />
                 </div>}
             </TrackVisibility>
           </Col>
