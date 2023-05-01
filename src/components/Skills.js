be773a7a9634d4b16d0dbb5c1322083573424183
Willian <willian.m.sousa@gmail.com>
2023-05-01 15:16:32 -0300

    Drop Shadow, Skill Img

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,8 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import { Container, Row, Col } from "react-bootstrap";
+import skillImg from "../assets/img/skill-img.svg";
+import TrackVisibility from 'react-on-screen';
+
 
 export const Skills = () => {
   const responsive = {
@@ -30,33 +27,44 @@ export const Skills = () => {
 
   return (
     <section className="skill" id="skills">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="skill-bx wow zoomIn">
-              <p>Da configuração a otimização! Esse é o #social o plano pensado para facilitar o
-                dia a dia da comunicação nas redes sociais  da sua empresa!</p>
+      <Container>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="skill-bx wow zoomIn">
+                <p>Da configuração a otimização! Esse é o #social o plano pensado para facilitar o
+                  dia a dia da comunicação nas redes sociais  da sua empresa!</p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="skill-bx2 wow zoomIn">
+                <p id="corel1">Usamos nossas experiências dos últimos 2 anos e mais de 30 empresas testadas para
+                  criar um serviço que realmente funciona de acordo com a necessidade da sua empresa.</p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="skill-bx2 wow zoomIn">
+                <p id="corel1">O controle nas suas mãos! Elaboramos os relatórios de todo o processo, com definição
+                  de metas com foco em resultados.</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-12">
-            <div className="skill-bx2 wow zoomIn">
-              <p id="corel1">Usamos nossas experiências dos últimos 2 anos e mais de 30 empresas testadas para 
-                criar um serviço que realmente funciona de acordo com a necessidade da sua empresa.</p>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <div className="skill-bx2 wow zoomIn">
-              <p id="corel1">O controle nas suas mãos! Elaboramos os relatórios de todo o processo, com definição 
-                de metas com foco em resultados.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <img className="background-image-left" src={colorSharp} alt="Image" />
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={skillImg} alt="Header Img" />
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
     </section>
   )
 }
